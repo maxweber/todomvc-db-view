dfe175fffac99ea340f06398cdc980b91f8bd21a
Max <weber.maximilian@googlemail.com>
2019-07-07 12:57:54 +0200

    Added a Datomic schema for the example.

--- src/clj/todomvc_db_view/datomic/schema.clj
+++ src/clj/todomvc_db_view/datomic/schema.clj
@@ -5,4 +5,12 @@
 ;; Contains the Datomic schema for this application.
 
 (def schema
-  [])
+  [{:db/ident :todo/title
+    :db/valueType :db.type/string
+    :db/cardinality :db.cardinality/one
+    :db/doc "Title of the TODO entry."}
+
+   {:db/ident :todo/done
+    :db/valueType :db.type/boolean
+    :db/cardinality :db.cardinality/one
+    :db/doc "Marks the TODO entry as done."}])
