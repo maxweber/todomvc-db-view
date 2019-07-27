69bad554a85b17824a1383b8087430b037e4b5c2
Max <weber.maximilian@googlemail.com>
2019-07-27 14:39:17 +0200

    Latest Clojure version.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject todomvc-db-view "0.1.0-SNAPSHOT"
   :description "An example app that demonstrates the db-view approach and implements a frontend and a backend for the well-known todomvc example app."
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [http-kit "2.3.0"]
                  [ring/ring-core "1.7.1"]
                  [com.datomic/datomic-free "0.9.5697"]
