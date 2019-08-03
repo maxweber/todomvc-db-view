98f8daa0bb2d21d8b0b734eabfbc6f97720e7ec5
Max <weber.maximilian@googlemail.com>
2019-08-03 11:26:12 +0200

    Improved documentation.

--- src/clj/todomvc_db_view/db_view/notify.clj
+++ src/clj/todomvc_db_view/db_view/notify.clj
@@ -9,16 +9,21 @@
 ;; in the Datomic transaction log, which affect their current
 ;; logged-in user.
 ;;
-;; Thereby the client can refresh the `:db-view/output` as soon as it
-;; is notified by this API endpoint. HTTP long polling is used here to
-;; allow the server to push a message to the client. It is less
-;; complex to implement in comparison to WebSockets and the low
-;; latency / overhead of WebSockets is not required for this use case.
+;; Thereby the client can refresh the `:db-view/output` map as soon as
+;; it is notified by this API endpoint. HTTP long polling is used here
+;; to allow the server to push a message to the client. It is less
+;; complex to implement in comparison to WebSockets. Furthermore the
+;; low latency and reduced payload size of WebSockets is not required
+;; for this use case.
 
 (defonce client-listeners-state
+  ;; holds the httpkit channels of the clients, which are waiting for
+  ;; a db-view notify:
   (atom {}))
 
-(defn ring-handler [request]
+(defn ring-handler
+  "Ring-handler for the '/db-view/notify' API endpoint."
+  [request]
   (when (and (= (:request-method request) :post)
              (= (:uri request) "/db-view/notify"))
     ;; NOTE: for a production app add an authentication check here:
@@ -31,25 +36,25 @@
              channel)
       (httpkit/on-close channel
                         (fn [status]
-                          ;; remove the user's browser session
-                          ;; from the `client-listeners-state` as
-                          ;; soon as the channel is closed (user
-                          ;; has closed the browser tab or the
-                          ;; network connection was interrupted:
+                          ;; remove the user's browser session from
+                          ;; the `client-listeners-state` as soon as
+                          ;; the channel is closed, meaning the user
+                          ;; has closed the browser tab or the network
+                          ;; connection was interrupted:
                           (swap! client-listeners-state
                                  dissoc
                                  channel))))))
 
 (defn notify
   "A Datomic transaction listener that notifies all user browser
-   sessions, where the user were affected by the transaction of the
+   sessions, where the user was affected by the transaction of the
    `tx-report`."
   [tx-report]
   (let [basis-t (d/basis-t (:db-after tx-report))
         response (edn/response
                   {:db/basis-t basis-t})]
     ;; NOTE: for a production app only send notifications to the users
-    ;;       which are affected by this `tx-report`.
+    ;;       which are affected by this `tx-report`:
     (doseq [channel (vals @client-listeners-state)]
       (httpkit/send! channel
                      response))))
