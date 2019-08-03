4931b29a9234ad73d13c29616a0d824e6a85b63e
Max <weber.maximilian@googlemail.com>
2019-08-03 10:27:37 +0200

    Improved documentation.

--- src/clj/todomvc_db_view/command/crypto.clj
+++ src/clj/todomvc_db_view/command/crypto.clj
@@ -5,38 +5,36 @@
 
 ;; Concept:
 ;;
-;; Provides the tools to encrypt and decrypt command maps. The server
-;; uses encryption to prevent that the untrusted client can manipulate
-;; the command map or send fake requests to the '/command' API
-;; endpoint.
+;; Provides tools to encrypt and decrypt command maps. The server uses
+;; encryption to prevent that someone can manipulate the command map
+;; or send fake requests to the '/command' API endpoint.
 ;;
-;; The encryption variant is feasible since the db-view is also used
-;; for validation. Only the server creates command maps and the client
-;; does not need to add additonal entries to the command map. If the
-;; user likes to change his last name for example, the client adds the
-;; new last name to the `:db-view/input` and refreshes the
-;; db-view. The server can validate the new last name and return an
-;; encrypted command map with the new last name in the
-;; `:db-view/output`. The client can send this command map to the
-;; '/command' API endpoint to execute the renaming. In the case of a
-;; validation error the server would return it in the `:db-view/output`
-;; instead of returning an encrypted command map. Some validation
-;; types still needs to be re-checked on the server, for example if
-;; you like to ensure unique names. However the '/command' API
-;; endpoint can then tell the client via the response body to redo the
-;; validation step, so that the user sees the corresponding error
-;; message.
+;; Only the server creates command maps, they are returned as part of
+;; the `:db-view/output` response. The client does not need to add
+;; additonal entries to a command map. For example, if the user likes
+;; to change his last name, the client adds the new last name to the
+;; `:db-view/input` and refreshes the db-view. The server can validate
+;; the new last name and return an encrypted command map with the new
+;; last name in the `:db-view/output`. The client can send this
+;; command map to the '/command' API endpoint to execute the
+;; change. In the case of a validation error the server would return
+;; it in the `:db-view/output` instead of returning the encrypted
+;; command map. Some validation types still needs to be re-checked on
+;; the server, for example if you like to ensure unique names. However
+;; the '/command' API endpoint can then tell the client via the
+;; response body to redo the validation step, so that the user sees
+;; the corresponding error message.
 ;;
 ;; Also authorization aspects become simpler to handle with this
 ;; approach. If an user is not allowed to change his last name, the
 ;; server can just exclude the corresponding command map from the
 ;; db-view. The client UI only shows the corresponding button or input
-;; field, if the corresponding command map is in the
-;; `:db-view/output`. The '/command' API endpoint does not re-check if
-;; the user is allowed to do this command, since the user would not
-;; never receive the corresponding encrypted command via the
-;; `:db-view/output`. Expiration dates in the command maps can be used
-;; to enforce authorization changes in a given timeout.
+;; field, if the command map is in the `:db-view/output`. The
+;; '/command' API endpoint does not re-check, if the user is allowed
+;; to preform this command, since the user would not have received the
+;; corresponding encrypted command via the `:db-view/output` in the
+;; first place. Expiration dates in the command maps can be used to
+;; enforce authorization changes in a given timeout.
 
 (defonce the-key
   (delay
